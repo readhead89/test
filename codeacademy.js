e22888af7a6972cb1dd424be02bf5eb728242513
readhead89 <redhead@deeplay.io>
2023-05-24 11:39:44 +0300

    Добавил тестовые кейсы, на память

---
+++ codeacademy.js
@@ -0,0 +1,53 @@
+if ((randomNumber === 1)) {
+    console.log("It is certain");
+} else if ((randomNumber === 2)) {
+    console.log("It is decidedly so");
+} else if ((randomNumber === 3)) {
+    console.log("Reply hazy try again");
+} else if ((eightBall === 4)) {
+    console.log("Cannot predict now");
+} else if ((eightBall === 5)) {
+    console.log("Do not count on it");
+} else if ((eightBall === 6)) {
+    console.log("My sources say no");
+} else if ((eightBall === 7)) {
+    console.log("Outlook not so good");
+} else if ((eightBall === 8)) {
+    console.log("Signs point to yes");
+} else {
+    console.log(0);
+}
+console.log(eightBall);
+
+
+
+
+switch (randomNumber){
+    case (randomNumber=1):
+        console.log("It is certain");
+        break;
+    case (randomNumber=2):
+        console.log("It is decidedly so");
+        break;
+    case (randomNumber=3):
+        console.log("Reply hazy try again");
+        break;
+    case (randomNumber=4):
+        console.log("Cannot predict now");
+        break;
+    case (randomNumber=5):
+        console.log("Do not count on it");
+        break;
+    case (randomNumber=6):
+        console.log("My sources say no");
+        break;
+    case (randomNumber=7):
+        console.log("Outlook not so good");
+        break;
+    case (randomNumber=8):
+        console.log("Signs point to yes");
+        break;
+    default:
+        console.log(0);
+        break;
+}
