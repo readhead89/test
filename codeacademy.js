b795822cc9a126451dc691e8c1dc367eb560947e
readhead89 <redhead@deeplay.io>
2023-05-24 14:13:57 +0300

    Добавил материалы урока

--- codeacademy.js
+++ codeacademy.js
@@ -1,3 +1,4 @@
+//Примеры if / else if и switch
 if ((randomNumber === 1)) {
     console.log("It is certain");
 } else if ((randomNumber === 2)) {
@@ -18,9 +19,6 @@ if ((randomNumber === 1)) {
     console.log(0);
 }
 console.log(eightBall);
-
-
-
 
 switch (randomNumber){
     case (randomNumber=1):
@@ -51,3 +49,42 @@ switch (randomNumber){
         console.log(0);
         break;
 }
+/* Функциональные выражения.
+Допустим, у нас есть растение, которое нужно поливать раз в неделю по средам. Мы могли бы определить функциональное выражение, которое поможет нам проверить день недели и нужно ли поливать растение:
+Создайте переменную с именем, plantNeedsWater используя const ключевое слово variable.Назначьте анонимную функцию, которая принимает параметр day to plantNeedsWater.
+Теперь нам нужно добавить код в тело функции plantNeedsWater(): В теле функции добавьте if условие, которое проверяет day === 'Wednesday'.
+Если условное выражение истинно, внутри if блока кода используйте return ключевое слово для возврата true. В нерабочие дни 'Wednesday' должно plantNeedsWater()вернуться false:
+Добавьте else заявление после if заявления. Внутри else инструкции используйте return ключевое слово для возврата false.
+Вызовите plantNeedsWater()и передайте в 'Tuesday' качестве аргумента. Проверим, что plantNeedsWater()вернуло ожидаемое значение.
+Войдите plantNeedsWater('Tuesday')в консоль. Если это сработало правильно, вы должны увидеть false вход в консоль.
+ */
+const plantNeedsWater = function(day) {
+    if (day === 'Wednesday'){
+        return true
+    }
+    else
+        return false
+}
+plantNeedsWater('Tuesday')
+console.log (plantNeedsWater('Wednesday'))
+
+//Упрощенный вариант
+
+const plantNeedsWater1 = function(day) {
+    return day === 'Wednesday';
+}
+plantNeedsWater1('Tuesday')
+console.log (plantNeedsWater1('Wednesday'))
+
+//Стрелочые функции
+const plantNeedsWater3 = day => {
+    return day === 'Wednesday';
+};
+//Краткие функции стрелок тела
+/*
+const squareNum = (num) => {
+    return num * num;
+};
+const squareNum = num => num * num;
+ */
+const plantNeedsWater4 = day => day === 'Wednesday'
