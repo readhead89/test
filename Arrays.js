9d6abe5f1bc314cb4ee0d49edcd176e4cedca841
readhead89 <redhead@deeplay.io>
2023-05-25 08:18:46 +0300

    Обновлен файл Arrays.js

--- Arrays.js
+++ Arrays.js
@@ -1,5 +1,6 @@
-//Массивы
 /*
+Shift + F9 запуск кода
+МАССИВЫ
 Каждый элемент в массиве имеет пронумерованную позицию, известную как его индекс . Мы можем получить доступ к отдельным элементам, используя их индекс, что аналогично ссылке на элемент в списке на основе позиции элемента.
 Массивы в JavaScript имеют нулевой индекс , то есть позиции начинают отсчет с, 0а не с 1. Следовательно, первый элемент массива будет в позиции 0. Давайте посмотрим, как мы можем получить доступ к элементу в массиве
 Во фрагменте кода выше:
@@ -22,3 +23,55 @@ console.log (famousSayings[3])
 let groceryList = ['bread', 'tomatoes', 'milk'];
 groceryList[1]= 'avocados'
 
+/*
+Свойство .length
+Одно из встроенных свойств массива — lengthи возвращает количество элементов в массиве. Мы обращаемся к .length свойству так же, как и к строкам.
+ */
+
+const objectives = ['Learn a new language', 'Read 52 books', 'Run a marathon'];
+console.log (objectives.length)
+
+/*
+Метод .push()
+Один метод .push() позволяет нам добавлять элементы в конец массива. Вот пример того, как это используется
+ */
+const chores = ['wash dishes', 'do laundry', 'take out trash'];
+chores.push('fly airplane', 'take cake')
+console.log(chores)
+
+/*Метод .pop()
+Другой метод массива .pop() удаляет последний элемент массива.
+ */
+const chores1 = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+chores1.pop()
+console.log (chores1)
+
+/*
+Другие методы массива. Некоторые методы массивов, доступные разработчикам JavaScript, включают: .join(), .slice(), .splice(), .shift(), .unshift() и .concat()многие другие. Использование этих встроенных методов упрощает выполнение некоторых общих задач при работе с массивами.
+
+ */
+/*Используйте .shift()метод для удаления первого элемента из массива groceryList. Зарегистрируйте новое groceryList в консоли. Читайте об этом .shift()в Документах https://www.codecademy.com/resources/docs/javascript/arrays/shift
+ */
+
+// Метод .shift() удаляет первый элемент массива
+const groceryList1 = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+groceryList1.shift()
+console.log(groceryList1)
+
+//Метод .unshift() добавляет первый элемент в массив
+const groceryList2 = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+groceryList2.unshift('popcorn')
+console.log(groceryList2)
+
+/* Метод массива .slice()
+Возвращает неглубокую копию всего массива или его части без изменения оригинала. Неглубокая копия дублирует содержимое объекта в новый экземпляр по ссылке; поэтому изменения в копии не отражаются в оригинале.
+ */
+const groceryList3 = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+groceryList3.unshift('popcorn')
+console.log (groceryList3.slice(1,4))
+console.log (groceryList3)
+
+// Метод массива .indexOf() возвращает индекс массива
+const groceryList4 = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+const pastaIndex =groceryList4.indexOf('pasta')
+console.log (pastaIndex)
--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function addName() {
     console.log('1')
 }
 addName()
+
+
+const groceryList3 = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+groceryList3.unshift('popcorn')
+console.log (groceryList3.slice(1,5))
+console.log (groceryList3)
