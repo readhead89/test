46bd7d3d3bfaae50d1665d3b6b9cdf3ba62b919c
readhead89 <redhead@deeplay.io>
2023-06-06 07:27:56 +0300

    Добавлены новые файлы Begin.js и typeof.js

---
+++ LearnBeginnerJavaScript/typeof.js
@@ -0,0 +1,12 @@
+/*
+оператор typeof
+При написании кода может быть полезно отслеживать типы данных переменных в вашей программе. Если вам нужно проверить тип данных значения переменной, вы можете использовать typeo fоператор.
+Оператор typeof проверяет значение справа от него и возвращает или передает обратно строку типа данных.
+const unknown1 = 'foo';
+console.log(typeof unknown1); // Output: string
+const unknown2 = 10;
+console.log(typeof unknown2); // Output: number
+const unknown3 = true;
+console.log(typeof unknown3); // Output: boolean
+Разберем первый пример. Поскольку значение unknown1равно 'foo', typeof unknown1будет возвращена строка 'string'.
+ */
